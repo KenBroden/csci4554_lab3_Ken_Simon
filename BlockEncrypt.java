dfbaccb2ea6347b4466efa74d828ab154c6d4872
Ken Broden <brod0268@morris.umn.edu>
2024-10-07 16:26:23 -0500

    Started block encrypt file
    convert to binary

---
+++ BlockEncrypt.java
@@ -0,0 +1,18 @@
+
+// Encryption for Block cipher
+public class BlockEncrypt {
+    
+    // Convert ascii to binary
+    public static String asciiToBinary(String asciiString) {
+        byte[] bytes = asciiString.getBytes();
+        StringBuilder binary = new StringBuilder();
+        for (byte b : bytes) {
+            int val = b;
+            for (int i = 0; i < 8; i++) {
+                binary.append((val & 128) == 0 ? 0 : 1);
+                val <<= 1;
+            }
+        }
+        return binary.toString();
+    }
+}
