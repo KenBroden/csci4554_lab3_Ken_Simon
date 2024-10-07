d6521e0127f9e0896d06c7fd6cd8b75415515bf5
Simon H-M <simonhm31@gmail.com>
2024-10-07 16:45:03 -0500

    changed output to array

--- convertStringToASCII.js
+++ convertStringToASCII.js
@@ -1,13 +1,16 @@
 
+let str = "abcd";
+
+
 // Converting a string to an array of ASCII decimal characters
 let convertStringToAsciiDecimalCharacters = function(str){
   let l = str.length;
   let arr = [];
   for (let i = 0; i < l; i++) {
-    arr = str[i].charCodeAt(0);
-    console.log(arr);  
+    arr[i] = str[i].charCodeAt(0);
   }
+  console.log(arr);  
 }
    
-let str = "abcd";
-convertStringToAsciiDecimalCharacters(str);
+const decimalASCIICharacters = convertStringToAsciiDecimalCharacters(str);
+
