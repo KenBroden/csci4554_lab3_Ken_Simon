26bba1cdb6a61ed503988205028db6283477e8d3
Simon H-M <harr3247@morris.umn.edu>
2024-10-08 13:29:40 -0500

    added better comments

--- convertStringToASCII.js
+++ convertStringToASCII.js
@@ -1,6 +1,9 @@
 
-let str = "abcd";
+let str = "%*#12";
+console.log("INPUT STRING:'" + str + "'")
 
+let key = "hello";
+console.log("KEY:'" + key  + "'")
 
 // Converting a string to an array of ASCII decimal characters
 let convertStringToAsciiDecimalCharacters = function(str){
@@ -13,19 +16,19 @@ let convertStringToAsciiDecimalCharacters = function(str){
 }
   
 const decimalArray = convertStringToAsciiDecimalCharacters(str);
-console.log("The input string as an array of decimal characters is " + decimalArray)
+console.log("The input string as an array of decimal characters is ", decimalArray)
 
-
+//Converting an array of decimal characters to binary characters
 let convertDecimalToAsciiBinaryCharacters = function(decimalArray){
   let binaryArray = [];
   for(let i = 0; i < decimalArray.length; i++){
-    binaryArray[i] = decimalArray[i].toString(2).padStart(8, '0');
+    let binaryString =  decimalArray[i].toString(2).padStart(8, '0');
+    binaryArray[i] = binaryString;
   }
-
-
   return binaryArray;
 }
 
-const binaryArray = convertDecimalToAsciiBinaryCharacters([2, 5, 32, 78])
-console.log("The input string as an array of binary characters is " + binaryArray)
+const binaryArray = convertDecimalToAsciiBinaryCharacters(decimalArray)
+console.log("The input string as an array of binary characters is", binaryArray)
+
 
